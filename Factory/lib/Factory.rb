205ae883f65292a45f6bfa8922146f5c6705d50c
MEGApixel23 <megapixel23@ya.ru>
2015-05-14 16:32:41 +0300

    'Fix'

--- Factory/lib/Factory.rb
+++ Factory/lib/Factory.rb
@@ -1,12 +1,13 @@
 require_relative 'Factory/Factory.rb'
 require_relative 'Factory/version.rb'
 
-=begin
+
 Test = Factory::Factory.new(:name) {p 'New class created'}
 test = Test.new 123
 
+=begin
 puts test[0]
 puts test['name']
 puts test.name
-puts test.namebundle
 =end
+
